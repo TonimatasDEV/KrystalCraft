2523e3ceba9c4646d75649fbf8e4674b1c208033
TonimatasMC <amatascubero@gmail.com>
2022-04-17 01:42:25 +0200

    master

--- src/main/java/net/tonimatasmc/krystalcraft/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/tonimatasmc/krystalcraft/world/feature/ModConfiguredFeatures.java
@@ -12,32 +12,47 @@ import net.tonimatasmc.krystalcraft.config.KrystalCraftModCommonConfigs;
 import java.util.List;
 
 public class ModConfiguredFeatures {
-    public static final List<OreConfiguration.TargetBlockState> OVERWORLD_JADE_ORES = List.of(
-            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.JADE_ORE.get().defaultBlockState()),
-            OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.JADE_ORE.get().defaultBlockState()));
+        public static final List<OreConfiguration.TargetBlockState> OVERWORLD_JADE_ORES = List.of(
+                OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.JADE_ORE.get().defaultBlockState()),
+                OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.JADE_ORE.get().defaultBlockState()));
+    
+        public static final List<OreConfiguration.TargetBlockState> OVERWORLD_SILVER_ORES = List.of(
+                OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.SILVER_ORE.get().defaultBlockState()),
+                OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.SILVER_ORE.get().defaultBlockState()));
+    
+        public static final List<OreConfiguration.TargetBlockState> OVERWORLD_TOPAZ_ORES = List.of(
+                OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.TOPAZ_ORE.get().defaultBlockState()),
+                OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.TOPAZ_ORE.get().defaultBlockState()));
 
-    public static final List<OreConfiguration.TargetBlockState> OVERWORLD_SILVER_ORES = List.of(
-            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.SILVER_ORE.get().defaultBlockState()),
-            OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.SILVER_ORE.get().defaultBlockState()));
+        public static final List<OreConfiguration.TargetBlockState> OVERWORLD_LEAD_ORES = List.of(
+                OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.LEAD_ORE.get().defaultBlockState()),
+                OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.LEAD_ORE.get().defaultBlockState()));
+            
+        public static final List<OreConfiguration.TargetBlockState> OVERWORLD_TIN_ORES = List.of(
+                OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.TIN_ORE.get().defaultBlockState()),
+                OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.TIN_ORE.get().defaultBlockState()));
 
-    public static final List<OreConfiguration.TargetBlockState> OVERWORLD_TOPAZ_ORES = List.of(
-            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.TOPAZ_ORE.get().defaultBlockState()),
-            OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.TOPAZ_ORE.get().defaultBlockState()));
+        public static final List<OreConfiguration.TargetBlockState> OVERWORLD_PLATINUM_ORES = List.of(
+                OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.PLATINUM_ORE.get().defaultBlockState()),
+                OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.PLATINUM_ORE.get().defaultBlockState()));
 
-    public static final List<OreConfiguration.TargetBlockState> OVERWORLD_LEAD_ORES = List.of(
-            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.LEAD_ORE.get().defaultBlockState()),
-            OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.LEAD_ORE.get().defaultBlockState()));
 //----------------------------------------------------------------------------------------------------------------------
 
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> JADE_ORE = FeatureUtils.register("jade_ore",
+        public static final Holder<ConfiguredFeature<OreConfiguration, ?>> JADE_ORE = FeatureUtils.register("jade_ore",
             Feature.ORE, new OreConfiguration(OVERWORLD_JADE_ORES, KrystalCraftModCommonConfigs.JADE_ORE_VEIN_SIZE.get()));
 
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> SILVER_ORE = FeatureUtils.register("silver_ore",
+        public static final Holder<ConfiguredFeature<OreConfiguration, ?>> SILVER_ORE = FeatureUtils.register("silver_ore",
             Feature.ORE, new OreConfiguration(OVERWORLD_SILVER_ORES, KrystalCraftModCommonConfigs.SILVER_ORE_VEIN_SIZE.get()));
 
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> TOPAZ_ORE = FeatureUtils.register("topaz_ore",
+        public static final Holder<ConfiguredFeature<OreConfiguration, ?>> TOPAZ_ORE = FeatureUtils.register("topaz_ore",
             Feature.ORE, new OreConfiguration(OVERWORLD_TOPAZ_ORES, KrystalCraftModCommonConfigs.TOPAZ_ORE_VEIN_SIZE.get()));
 
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> LEAD_ORE = FeatureUtils.register("lead_ore",
+        public static final Holder<ConfiguredFeature<OreConfiguration, ?>> LEAD_ORE = FeatureUtils.register("lead_ore",
             Feature.ORE, new OreConfiguration(OVERWORLD_LEAD_ORES, KrystalCraftModCommonConfigs.LEAD_ORE_VEIN_SIZE.get()));
+
+        public static final Holder<ConfiguredFeature<OreConfiguration, ?>> TIN_ORE = FeatureUtils.register("tin_ore",
+            Feature.ORE, new OreConfiguration(OVERWORLD_TIN_ORES, KrystalCraftModCommonConfigs.TIN_ORE_VEIN_SIZE.get()));
+
+        public static final Holder<ConfiguredFeature<OreConfiguration, ?>> PLATINUM_ORE = FeatureUtils.register("platinum_ore",
+            Feature.ORE, new OreConfiguration(OVERWORLD_PLATINUM_ORES, KrystalCraftModCommonConfigs.PLATINUM_ORE_VEIN_SIZE.get()));
 }
--- src/main/java/net/tonimatasmc/krystalcraft/world/gen/ModOreGeneration.java
+++ src/main/java/net/tonimatasmc/krystalcraft/world/gen/ModOreGeneration.java
@@ -1,15 +1,13 @@
 package net.tonimatasmc.krystalcraft.world.gen;
 
+import java.util.List;
+
 import net.minecraft.core.Holder;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.tonimatasmc.krystalcraft.world.feature.ModPlacedFeatures;
 
-import java.util.List;
-
-@SuppressWarnings("StatementWithEmptyBody")
 public class ModOreGeneration {
     public static void generateOres(final BiomeLoadingEvent event) {
         List<Holder<PlacedFeature>> base = event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES);
@@ -18,11 +16,7 @@ public class ModOreGeneration {
         base.add(ModPlacedFeatures.SILVER_ORE_PLACED);
         base.add(ModPlacedFeatures.TOPAZ_ORE_PLACED);
         base.add(ModPlacedFeatures.LEAD_ORE_PLACED);
-
-        if(event.getCategory() == Biome.BiomeCategory.THEEND) {
-        }
-
-        if(event.getCategory() == Biome.BiomeCategory.NETHER) {
-        }
+        base.add(ModPlacedFeatures.TIN_ORE_PLACED);
+        base.add(ModPlacedFeatures.PLATINUM_ORE_PLACED);
     }
 }
