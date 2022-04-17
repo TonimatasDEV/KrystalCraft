e25d84004e76ac67eed264983a18084941bb5ea8
TonimatasMC <amatascubero@gmail.com>
2022-04-17 02:21:21 +0200

    master

--- src/main/java/net/tonimatasmc/krystalcraft/config/KrystalCraftModCommonConfigs.java
+++ src/main/java/net/tonimatasmc/krystalcraft/config/KrystalCraftModCommonConfigs.java
@@ -8,76 +8,44 @@ public class KrystalCraftModCommonConfigs {
 
     public static final ForgeConfigSpec.ConfigValue<Integer> JADE_ORE_VEINS_PER_CHUNK;
     public static final ForgeConfigSpec.ConfigValue<Integer> JADE_ORE_VEIN_SIZE;
-    public static final ForgeConfigSpec.ConfigValue<Integer> JADE_ORE_MINIMUN_HEIGHT;
-    public static final ForgeConfigSpec.ConfigValue<Integer> JADE_ORE_MAXIMUN_HEIGHT;
 
     public static final ForgeConfigSpec.ConfigValue<Integer> SILVER_ORE_VEINS_PER_CHUNK;
     public static final ForgeConfigSpec.ConfigValue<Integer> SILVER_ORE_VEIN_SIZE;
-    public static final ForgeConfigSpec.ConfigValue<Integer> SILVER_ORE_MINIMUN_HEIGHT;
-    public static final ForgeConfigSpec.ConfigValue<Integer> SILVER_ORE_MAXIMUN_HEIGHT;
 
     public static final ForgeConfigSpec.ConfigValue<Integer> TOPAZ_ORE_VEINS_PER_CHUNK;
     public static final ForgeConfigSpec.ConfigValue<Integer> TOPAZ_ORE_VEIN_SIZE;
-    public static final ForgeConfigSpec.ConfigValue<Integer> TOPAZ_ORE_MINIMUN_HEIGHT;
-    public static final ForgeConfigSpec.ConfigValue<Integer> TOPAZ_ORE_MAXIMUN_HEIGHT;
 
     public static final ForgeConfigSpec.ConfigValue<Integer> LEAD_ORE_VEINS_PER_CHUNK;
     public static final ForgeConfigSpec.ConfigValue<Integer> LEAD_ORE_VEIN_SIZE;
-    public static final ForgeConfigSpec.ConfigValue<Integer> LEAD_ORE_MINIMUN_HEIGHT;
-    public static final ForgeConfigSpec.ConfigValue<Integer> LEAD_ORE_MAXIMUN_HEIGHT;
 
     public static final ForgeConfigSpec.ConfigValue<Integer> TIN_ORE_VEINS_PER_CHUNK;
     public static final ForgeConfigSpec.ConfigValue<Integer> TIN_ORE_VEIN_SIZE;
-    public static final ForgeConfigSpec.ConfigValue<Integer> TIN_ORE_MINIMUN_HEIGHT;
-    public static final ForgeConfigSpec.ConfigValue<Integer> TIN_ORE_MAXIMUN_HEIGHT;
 
     public static final ForgeConfigSpec.ConfigValue<Integer> PLATINUM_ORE_VEINS_PER_CHUNK;
     public static final ForgeConfigSpec.ConfigValue<Integer> PLATINUM_ORE_VEIN_SIZE;
-    public static final ForgeConfigSpec.ConfigValue<Integer> PLATINUM_ORE_MINIMUN_HEIGHT;
-    public static final ForgeConfigSpec.ConfigValue<Integer> PLATINUM_ORE_MAXIMUN_HEIGHT;
 
     static {
         BUILDER.push("Configs for KrystalCraft");
 
         JADE_ORE_VEINS_PER_CHUNK = BUILDER.comment("How many Jade Ore Veins spawn per chunk!").define("Veins of Jade Ore Per Chunk", 3);
-        JADE_ORE_VEIN_SIZE = BUILDER.comment("How many Jade Ore Blocks spawn in one Vein!").defineInRange("Vein Size of Jade Ore ", 3, 1, 2);
-        JADE_ORE_MINIMUN_HEIGHT = BUILDER.comment().define("Minimum spawning height", 30);
-        JADE_ORE_MAXIMUN_HEIGHT = BUILDER.comment().define("Maximun spawning height", 90);
+        JADE_ORE_VEIN_SIZE = BUILDER.comment("How many Jade Ore Blocks spawn in one Vein!").defineInRange("Vein Size of Jade Ore", 3, 1, 2);
         
-        BUILDER.comment("----------------------------------------------------------------------");
 
         SILVER_ORE_VEINS_PER_CHUNK = BUILDER.comment("How many Silver Ore Veins spawn per chunk!").define("Veins of Silver Ore Per Chunk", 4);
         SILVER_ORE_VEIN_SIZE = BUILDER.comment("How many Silver Ore Blocks spawn in one Vein!").defineInRange("Vein Size of Silver Ore", 8, 1, 8);
-        SILVER_ORE_MINIMUN_HEIGHT = BUILDER.comment().define("Minimum spawning height", 70);
-        SILVER_ORE_MAXIMUN_HEIGHT = BUILDER.comment().define("Maximun spawning height", 140);
-
-        BUILDER.comment("----------------------------------------------------------------------");
 
         TOPAZ_ORE_VEINS_PER_CHUNK = BUILDER.comment("How many Topaz Ore Veins spawn per chunk!").define("Veins of Topaz Ore Per Chunk", 4);
         TOPAZ_ORE_VEIN_SIZE = BUILDER.comment("How many Topaz Ore Blocks spawn in one Vein!").defineInRange("Vein Size of Topaz Ore", 5, 1, 5);
-        TOPAZ_ORE_MINIMUN_HEIGHT = BUILDER.comment().define("Minimum spawning height", 40);
-        TOPAZ_ORE_MAXIMUN_HEIGHT = BUILDER.comment().define("Maximun spawning height", 90);
-
-        BUILDER.comment("----------------------------------------------------------------------");
 
         LEAD_ORE_VEINS_PER_CHUNK = BUILDER.comment("How many Lead Ore Veins spawn per chunk!").define("Veins of Lead Ore Per Chunk", 3);
         LEAD_ORE_VEIN_SIZE = BUILDER.comment("How many Lead Ore Blocks spawn in one Vein!").defineInRange("Vein of Lead Ore Size", 4, 1, 4);
-        LEAD_ORE_MINIMUN_HEIGHT = BUILDER.comment().define("Minimum spawning height", 0);
-        LEAD_ORE_MAXIMUN_HEIGHT = BUILDER.comment().define("Maximun spawning height", 100);
-
-        BUILDER.comment("----------------------------------------------------------------------");
 
         TIN_ORE_VEINS_PER_CHUNK = BUILDER.comment("How many Tin Ore Veins spawn per chunk!").define("Veins of Tin Per Chunk", 4);
-        TIN_ORE_VEIN_SIZE = BUILDER.comment("How many Tin Ore Blocks spawn in one Vein!").defineInRange("Vein Size of Tin", 9, 1, 10);
-        TIN_ORE_MINIMUN_HEIGHT = BUILDER.comment().define("Minimum spawning height", 100);
-        TIN_ORE_MAXIMUN_HEIGHT = BUILDER.comment().define("Maximun spawning height", 200);
+        TIN_ORE_VEIN_SIZE = BUILDER.comment("How many Tin Ore Blocks spawn in one Vein!").defineInRange("Vein Size of Tin Ore", 9, 1, 10);
 
-        BUILDER.comment("----------------------------------------------------------------------");
 
-        PLATINUM_ORE_VEINS_PER_CHUNK = BUILDER.comment("How many Platinum Ore Veins spawn per chunk!").define("Veins of Platinum Per Chunk", 2);
-        PLATINUM_ORE_VEIN_SIZE = BUILDER.comment("How many Platinum Ore Blocks spawn in one Vein!").defineInRange("Vein Size of Platinum", 1, 1, 2);
-        PLATINUM_ORE_MINIMUN_HEIGHT = BUILDER.comment().define("Minimum spawning height", 0);
-        PLATINUM_ORE_MAXIMUN_HEIGHT = BUILDER.comment().define("Maximun spawning height", 90);
+        PLATINUM_ORE_VEINS_PER_CHUNK = BUILDER.comment("How many Platinum Ore Veins spawn per chunk!").define("Veins of Platinum Ore Per Chunk", 2);
+        PLATINUM_ORE_VEIN_SIZE = BUILDER.comment("How many Platinum Ore Blocks spawn in one Vein!").defineInRange("Vein Size of Platinum Ore", 1, 1, 2);
 
         BUILDER.pop();
         SPEC = BUILDER.build();
--- src/main/java/net/tonimatasmc/krystalcraft/world/feature/ModPlacedFeatures.java
+++ src/main/java/net/tonimatasmc/krystalcraft/world/feature/ModPlacedFeatures.java
@@ -11,31 +11,31 @@ public class ModPlacedFeatures {
         public static final Holder<PlacedFeature> JADE_ORE_PLACED = PlacementUtils.register("jade_ore_placed",
                 ModConfiguredFeatures.JADE_ORE, ModOrePlacement.commonOrePlacement(
                         KrystalCraftModCommonConfigs.JADE_ORE_VEINS_PER_CHUNK.get(),
-                        HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(KrystalCraftModCommonConfigs.JADE_ORE_MINIMUN_HEIGHT.get()), VerticalAnchor.aboveBottom(KrystalCraftModCommonConfigs.JADE_ORE_MAXIMUN_HEIGHT.get()))));
+                        HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(30), VerticalAnchor.aboveBottom(90))));
 
         public static final Holder<PlacedFeature> SILVER_ORE_PLACED = PlacementUtils.register("silver_ore_placed",
                 ModConfiguredFeatures.SILVER_ORE, ModOrePlacement.commonOrePlacement(
                         KrystalCraftModCommonConfigs.SILVER_ORE_VEINS_PER_CHUNK.get(),
-                        HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(KrystalCraftModCommonConfigs.SILVER_ORE_MINIMUN_HEIGHT.get()), VerticalAnchor.aboveBottom(KrystalCraftModCommonConfigs.SILVER_ORE_MAXIMUN_HEIGHT.get()))));
+                        HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(70), VerticalAnchor.aboveBottom(140))));
 
         public static final Holder<PlacedFeature> TOPAZ_ORE_PLACED = PlacementUtils.register("topaz_ore_placed",
                 ModConfiguredFeatures.TOPAZ_ORE, ModOrePlacement.commonOrePlacement(
                         KrystalCraftModCommonConfigs.TOPAZ_ORE_VEINS_PER_CHUNK.get(),
-                        HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(KrystalCraftModCommonConfigs.TOPAZ_ORE_MINIMUN_HEIGHT.get()), VerticalAnchor.aboveBottom(KrystalCraftModCommonConfigs.TOPAZ_ORE_MAXIMUN_HEIGHT.get()))));
+                        HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(40), VerticalAnchor.aboveBottom(90))));
 
         public static final Holder<PlacedFeature> LEAD_ORE_PLACED = PlacementUtils.register("lead_ore_placed",
                 ModConfiguredFeatures.LEAD_ORE, ModOrePlacement.commonOrePlacement(
                         KrystalCraftModCommonConfigs.LEAD_ORE_VEINS_PER_CHUNK.get(),
-                        HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(KrystalCraftModCommonConfigs.LEAD_ORE_MINIMUN_HEIGHT.get()), VerticalAnchor.aboveBottom(KrystalCraftModCommonConfigs.LEAD_ORE_MAXIMUN_HEIGHT.get()))));
+                        HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(5), VerticalAnchor.aboveBottom(110))));
 
         public static final Holder<PlacedFeature> TIN_ORE_PLACED = PlacementUtils.register("tin_ore_placed",
                 ModConfiguredFeatures.TIN_ORE, ModOrePlacement.commonOrePlacement(
                          KrystalCraftModCommonConfigs.TIN_ORE_VEINS_PER_CHUNK.get(),
-                         HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(KrystalCraftModCommonConfigs.TIN_ORE_MINIMUN_HEIGHT.get()), VerticalAnchor.aboveBottom(KrystalCraftModCommonConfigs.TIN_ORE_MAXIMUN_HEIGHT.get()))));
+                         HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(100), VerticalAnchor.aboveBottom(200))));
         
         public static final Holder<PlacedFeature> PLATINUM_ORE_PLACED = PlacementUtils.register("tin_ore_placed",
                 ModConfiguredFeatures.PLATINUM_ORE, ModOrePlacement.commonOrePlacement(
                         KrystalCraftModCommonConfigs.PLATINUM_ORE_VEINS_PER_CHUNK.get(),
-                        HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(KrystalCraftModCommonConfigs.PLATINUM_ORE_MINIMUN_HEIGHT.get()), VerticalAnchor.aboveBottom(KrystalCraftModCommonConfigs.PLATINUM_ORE_MAXIMUN_HEIGHT.get()))));
+                        HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(5), VerticalAnchor.aboveBottom(90))));
                                 
 }
