788b1b63c9dc7cddd5c8b3ab8adc31bbc604d0b1
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2022-05-14 19:05:14 +0200

    KrystalCraft 0.0.2.1

---
+++ src/main/java/net/tonimatasmc/krystalcraft/enchantment/PlayerHeadsEnchantment.java
@@ -0,0 +1,48 @@
+package net.tonimatasmc.krystalcraft.enchantment;
+
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+
+import javax.annotation.Nullable;
+import java.util.Objects;
+
+public class PlayerHeadsEnchantment extends Enchantment {
+    protected PlayerHeadsEnchantment(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot... pApplicableSlots) {
+        super(pRarity, pCategory, pApplicableSlots);
+    }
+
+    @SuppressWarnings("unused")
+    @Override
+    public void doPostAttack(LivingEntity pAttacker, @Nullable Entity pTarget, int pLevel) {
+        if (!pAttacker.level.isClientSide()) {
+            ServerLevel world = (ServerLevel) pAttacker.level;
+            ServerPlayer player = ((ServerPlayer) pAttacker);
+            BlockPos position = Objects.requireNonNull(pTarget).blockPosition();
+
+            if (pLevel == 1) {
+                if (!(pTarget instanceof Player)) {
+                    ItemStack stack = new ItemStack(Items.PLAYER_HEAD, 1);
+                    stack.getOrCreateTag().putString("SkullOwner", pTarget.getName().toString());
+
+                    ItemEntity itementity = new ItemEntity(pTarget.level, position.getX(), position.getY(), position.getZ(), stack);
+                }
+            }
+        }
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 5;
+    }
+}
