7f36dc4482a4f5e13f7b2654603d9af9cd3bffe9
TonimatasDEV <amatascubero@gmail.com>
2023-03-25 20:41:32 +0100

    Optimize ModBlocksStateProvider

--- src/main/java/net/tonimatasdev/krystalcraft/datagen/ModBlocksStateProvider.java
+++ src/main/java/net/tonimatasdev/krystalcraft/datagen/ModBlocksStateProvider.java
@@ -1,8 +1,10 @@
 package net.tonimatasdev.krystalcraft.datagen;
 
 import net.minecraft.data.DataGenerator;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
 import net.tonimatasdev.krystalcraft.KrystalCraft;
 import net.tonimatasdev.krystalcraft.block.ModBlocks;
 
@@ -13,32 +15,8 @@ public class ModBlocksStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        simpleBlock(ModBlocks.BRONZE_BLOCK.get());
-        simpleBlock(ModBlocks.DEEPSLATE_EXPERIENCE_ORE.get());
-        simpleBlock(ModBlocks.DEEPSLATE_JADE_ORE.get());
-        simpleBlock(ModBlocks.DEEPSLATE_LEAD_ORE.get());
-        simpleBlock(ModBlocks.DEEPSLATE_PLATINUM_ORE.get());
-        simpleBlock(ModBlocks.DEEPSLATE_RUBY_ORE.get());
-        simpleBlock(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
-        simpleBlock(ModBlocks.DEEPSLATE_SILVER_ORE.get());
-        simpleBlock(ModBlocks.DEEPSLATE_TIN_ORE.get());
-        simpleBlock(ModBlocks.DEEPSLATE_TOPAZ_ORE.get());
-        simpleBlock(ModBlocks.EXPERIENCE_ORE.get());
-        simpleBlock(ModBlocks.JADE_BLOCK.get());
-        simpleBlock(ModBlocks.JADE_ORE.get());
-        simpleBlock(ModBlocks.LEAD_BLOCK.get());
-        simpleBlock(ModBlocks.LEAD_ORE.get());
-        simpleBlock(ModBlocks.PLATINUM_BLOCK.get());
-        simpleBlock(ModBlocks.PLATINUM_ORE.get());
-        simpleBlock(ModBlocks.RUBY_BLOCK.get());
-        simpleBlock(ModBlocks.RUBY_ORE.get());
-        simpleBlock(ModBlocks.SAPPHIRE_BLOCK.get());
-        simpleBlock(ModBlocks.SAPPHIRE_ORE.get());
-        simpleBlock(ModBlocks.SILVER_BLOCK.get());
-        simpleBlock(ModBlocks.SILVER_ORE.get());
-        simpleBlock(ModBlocks.TIN_BLOCK.get());
-        simpleBlock(ModBlocks.TIN_ORE.get());
-        simpleBlock(ModBlocks.TOPAZ_BLOCK.get());
-        simpleBlock(ModBlocks.TOPAZ_ORE.get());
+        for (RegistryObject<Block> block : ModBlocks.BLOCKS.getEntries()) {
+            simpleBlock(block.get());
+        }
     }
 }
