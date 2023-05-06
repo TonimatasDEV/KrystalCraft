15752fef94721c0fa65ef1f6306024301b5dfa83
TonimatasDEV <amatascubero@gmail.com>
2023-05-06 15:23:41 +0200

    Add world gen to data generators

--- src/main/java/net/tonimatasdev/krystalcraft/datagen/DataGenerators.java
+++ src/main/java/net/tonimatasdev/krystalcraft/datagen/DataGenerators.java
@@ -5,15 +5,12 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.registries.VanillaRegistries;
-import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.tonimatasdev.krystalcraft.KrystalCraft;
-import net.tonimatasdev.krystalcraft.world.feature.ModFeatures;
 
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 @Mod.EventBusSubscriber(modid = KrystalCraft.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -32,6 +29,6 @@ public class DataGenerators {
         ModBlockTagsProvider blockTagsProvider = new ModBlockTagsProvider(packOutput, completablefuture, existingFileHelper);
         generator.addProvider(true, blockTagsProvider);
         generator.addProvider(true, new ModItemTagsProvider(packOutput, completablefuture, blockTagsProvider.contentsGetter(), existingFileHelper));
-        generator.addProvider(true, new DatapackBuiltinEntriesProvider(packOutput, completablefuture, ModFeatures.BUILDER, Set.of("krystalcraft")));
+        generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, completablefuture));
     }
 }
