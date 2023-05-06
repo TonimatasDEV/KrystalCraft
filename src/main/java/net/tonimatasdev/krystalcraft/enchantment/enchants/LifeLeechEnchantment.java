e53f6ed095a5aa19a922909e3c13ef1a49bc6bb9
TonimatasDEV <amatascubero@gmail.com>
2023-05-06 16:07:20 +0200

    Optimize all enchantments

--- src/main/java/net/tonimatasdev/krystalcraft/enchantment/enchants/LifeLeechEnchantment.java
+++ src/main/java/net/tonimatasdev/krystalcraft/enchantment/enchants/LifeLeechEnchantment.java
@@ -1,51 +1,30 @@
 package net.tonimatasdev.krystalcraft.enchantment.enchants;
 
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 
 public class LifeLeechEnchantment extends Enchantment {
-    public LifeLeechEnchantment(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot... pApplicableSlots) {
-        super(pRarity, pCategory, pApplicableSlots);
+    public LifeLeechEnchantment() {
+        super(Enchantment.Rarity.RARE, EnchantmentCategory.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
     }
 
     @Override
-    public void doPostAttack(LivingEntity pAttacker, @Nullable Entity pTarget, int pLevel) {
-        if (!pAttacker.level.isClientSide()) {
-            if (pLevel == 1) {
-                float health = pAttacker.getHealth();
-                pAttacker.setHealth((float) (health + 0.1));
-            }
-
-            if (pLevel == 2) {
-                float health = pAttacker.getHealth();
-                pAttacker.setHealth((float) (health + 0.2));
-            }
-
-            if (pLevel == 3) {
-                float health = pAttacker.getHealth();
-                pAttacker.setHealth((float) (health + 0.3));
-            }
-
-            if (pLevel == 4) {
-                float health = pAttacker.getHealth();
-                pAttacker.setHealth((float) (health + 0.4));
-            }
-
-            if (pLevel == 5) {
-                float health = pAttacker.getHealth();
-                pAttacker.setHealth((float) (health + 0.5));
-            }
+    public void doPostAttack(@NotNull LivingEntity attacker, @Nullable Entity target, int level) {
+        if (attacker instanceof ServerPlayer player) {
+            player.setHealth(attacker.getHealth() + ((float) level /10));
         }
     }
 
     @Override
     public int getMaxLevel() {
-        return 5;
+        return 3;
     }
 }
