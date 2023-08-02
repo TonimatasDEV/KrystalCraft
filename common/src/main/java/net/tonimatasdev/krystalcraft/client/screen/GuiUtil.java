62bc14441b2e9a8252d149025416702b24d81bb4
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-08-03 00:49:25 +0200

    Add overlays and change the energy color to red

---
+++ common/src/main/java/net/tonimatasdev/krystalcraft/client/screen/GuiUtil.java
@@ -0,0 +1,45 @@
+package net.tonimatasdev.krystalcraft.client.screen;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.resources.ResourceLocation;
+import net.tonimatasdev.krystalcraft.KrystalCraft;
+
+import java.awt.*;
+
+public class GuiUtil {
+    public static final int ENERGY_WIDTH = 13;
+    public static final int ENERGY_HEIGHT = 46;
+
+    public static final ResourceLocation ENERGY_TEXTURE = new ResourceLocation(KrystalCraft.MOD_ID, "textures/gui/overlay/energy_full.png");
+
+    public static boolean isHovering(Rectangle bounds, double x, double y) {
+        double left = bounds.getX();
+        double right = left + bounds.getWidth();
+        double top = bounds.getY();
+        double bottom = top + bounds.getHeight();
+        return left <= x && x < right && top <= y && y < bottom;
+    }
+
+    public static void drawEnergy(GuiGraphics graphics, int x, int y, long energy, long maxEnergy) {
+        double ratio = maxEnergy > 0 ? (energy / (float) maxEnergy) : 0;
+        drawVertical(graphics, x, y, ENERGY_WIDTH, ENERGY_HEIGHT, ENERGY_TEXTURE, ratio);
+    }
+
+    public static void drawVertical(GuiGraphics graphics, int x, int y, int width, int height, ResourceLocation resource, double ratio) {
+        int ratioHeight = (int) Math.ceil(height * ratio);
+        int remainHeight = height - ratioHeight;
+        graphics.blit(resource, x, y + remainHeight, 0, remainHeight, width, ratioHeight, width, height);
+    }
+
+    public static void drawEnergyTooltip(GuiGraphics graphics, long energy, long energyCapacity, int mouseX, int mouseY) {
+        graphics.renderTooltip(Minecraft.getInstance().font, Component.literal(energy + "/" + energyCapacity).setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)), mouseX, mouseY);
+    }
+
+    public static Rectangle getEnergyBounds(int x, int y) {
+        return new Rectangle(x, y, ENERGY_WIDTH, ENERGY_HEIGHT);
+    }
+}
