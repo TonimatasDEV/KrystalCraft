ec9229c513a25728f0286bfb35f8e808787da16d
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-08-05 20:22:17 +0200

    Add UpgradeItem for upgrades and change combining factory gui texture

---
+++ common/src/main/java/net/tonimatasdev/krystalcraft/item/custom/UpgradeItem.java
@@ -0,0 +1,9 @@
+package net.tonimatasdev.krystalcraft.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class UpgradeItem extends Item {
+    public UpgradeItem(Properties properties) {
+        super(properties);
+    }
+}
