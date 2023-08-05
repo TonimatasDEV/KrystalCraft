46e50078d37ebd7f3725180bc6bb11dae32ae44f
TonimatasDEV <amatascubero@gmail.com>
2023-08-05 20:18:41 +0200

    Only certain items can be placed.

--- common/src/main/java/net/tonimatasdev/krystalcraft/menu/CombiningFactoryMenu.java
+++ common/src/main/java/net/tonimatasdev/krystalcraft/menu/CombiningFactoryMenu.java
@@ -3,9 +3,12 @@ package net.tonimatasdev.krystalcraft.menu;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
 import net.tonimatasdev.krystalcraft.blockentity.CombiningFactoryBlockEntity;
+import net.tonimatasdev.krystalcraft.item.custom.UpgradeItem;
 import net.tonimatasdev.krystalcraft.networking.NetworkHandler;
 import net.tonimatasdev.krystalcraft.networking.packet.messages.ClientboundMachineInfoPacket;
+import net.tonimatasdev.krystalcraft.registry.ModItems;
 import net.tonimatasdev.krystalcraft.registry.ModMenus;
 
 import java.util.List;
@@ -18,9 +21,32 @@ public class CombiningFactoryMenu extends FactoryMenu<CombiningFactoryBlockEntit
     public CombiningFactoryMenu(int syncId, Inventory inventory, CombiningFactoryBlockEntity blockEntity) {
         super(ModMenus.COMBINING_FACTORY_MENU.get(), syncId, inventory, blockEntity,
                 new Slot[]{
-                        new Slot(blockEntity, 0, 70, 6),
-                        new Slot(blockEntity, 1, 90, 6),
-                        new Slot(blockEntity, 2, 80, 50)
+                        new Slot(blockEntity, 0, 80, -8),
+                        new Slot(blockEntity, 1, 80, 9),
+                        new Slot(blockEntity, 2, 80, 50) {
+                            @Override
+                            public boolean mayPlace(ItemStack itemStack) {
+                                return false;
+                            }
+                        },
+                        new Slot(blockEntity, 3, 129, 41) {
+                            @Override
+                            public boolean mayPlace(ItemStack itemStack) {
+                                return itemStack.getItem() == ModItems.BATTERY.get();
+                            }
+                        },
+                        new Slot(blockEntity, 4, 106, -12) {
+                            @Override
+                            public boolean mayPlace(ItemStack itemStack) {
+                                return itemStack.getItem() instanceof UpgradeItem;
+                            }
+                        },
+                        new Slot(blockEntity, 5, 126, -12) {
+                            @Override
+                            public boolean mayPlace(ItemStack itemStack) {
+                                return itemStack.getItem() instanceof UpgradeItem;
+                            }
+                        }
                 });
     }
 
--- common/src/main/java/net/tonimatasdev/krystalcraft/menu/CrushingFactoryMenu.java
+++ common/src/main/java/net/tonimatasdev/krystalcraft/menu/CrushingFactoryMenu.java
@@ -3,9 +3,12 @@ package net.tonimatasdev.krystalcraft.menu;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
 import net.tonimatasdev.krystalcraft.blockentity.CrushingFactoryBlockEntity;
+import net.tonimatasdev.krystalcraft.item.custom.UpgradeItem;
 import net.tonimatasdev.krystalcraft.networking.NetworkHandler;
 import net.tonimatasdev.krystalcraft.networking.packet.messages.ClientboundMachineInfoPacket;
+import net.tonimatasdev.krystalcraft.registry.ModItems;
 import net.tonimatasdev.krystalcraft.registry.ModMenus;
 
 import java.util.List;
@@ -19,7 +22,30 @@ public class CrushingFactoryMenu extends FactoryMenu<CrushingFactoryBlockEntity>
         super(ModMenus.CRUSHING_FACTORY_MENU.get(), syncId, inventory, blockEntity,
                 new Slot[]{
                         new Slot(blockEntity, 0, 80, 0),
-                        new Slot(blockEntity, 1, 80, 50)
+                        new Slot(blockEntity, 1, 80, 50) {
+                            @Override
+                            public boolean mayPlace(ItemStack itemStack) {
+                                return false;
+                            }
+                        },
+                        new Slot(blockEntity, 2, 129, 41) {
+                            @Override
+                            public boolean mayPlace(ItemStack itemStack) {
+                                return itemStack.getItem() == ModItems.BATTERY.get();
+                            }
+                        },
+                        new Slot(blockEntity, 3, 106, -12) {
+                            @Override
+                            public boolean mayPlace(ItemStack itemStack) {
+                                return itemStack.getItem() instanceof UpgradeItem;
+                            }
+                        },
+                        new Slot(blockEntity, 4, 126, -12) {
+                            @Override
+                            public boolean mayPlace(ItemStack itemStack) {
+                                return itemStack.getItem() instanceof UpgradeItem;
+                            }
+                        }
                 });
     }
 
