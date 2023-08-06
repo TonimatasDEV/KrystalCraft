6f674e679e1db863ef0e23ef5fde701b27aa0dce
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-08-06 23:11:53 +0200

    FFix problems with battery

--- common/src/main/java/net/tonimatasdev/krystalcraft/item/custom/BatteryItem.java
+++ common/src/main/java/net/tonimatasdev/krystalcraft/item/custom/BatteryItem.java
@@ -1,5 +1,6 @@
 package net.tonimatasdev.krystalcraft.item.custom;
 
+import earth.terrarium.botarium.common.energy.EnergyApi;
 import earth.terrarium.botarium.common.energy.base.BotariumEnergyItem;
 import earth.terrarium.botarium.common.energy.impl.SimpleEnergyContainer;
 import earth.terrarium.botarium.common.energy.impl.WrappedItemEnergyContainer;
@@ -10,14 +11,11 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
 import java.util.List;
 
 public class BatteryItem extends Item implements BotariumEnergyItem<WrappedItemEnergyContainer> {
-    protected WrappedItemEnergyContainer energyContainer;
 
     public BatteryItem(Properties properties) {
         super(properties);
@@ -25,12 +23,12 @@ public class BatteryItem extends Item implements BotariumEnergyItem<WrappedItemE
 
     @Override
     public WrappedItemEnergyContainer getEnergyStorage(ItemStack holder) {
-        return energyContainer == null ? energyContainer = new WrappedItemEnergyContainer(this.getDefaultInstance(), new SimpleEnergyContainer(7500)) : this.energyContainer;
+        return new WrappedItemEnergyContainer(holder, new SimpleEnergyContainer(7500));
     }
 
     @Override
     public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> list, TooltipFlag tooltipFlag) {
-        list.add(Component.literal(energyContainer.getStoredEnergy() + "/" + energyContainer.getMaxCapacity() + " FE").setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)));
+        list.add(Component.literal(getEnergyStorage(itemStack).getStoredEnergy() + "/" + getEnergyStorage(itemStack).getMaxCapacity() + " FE").setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)));
         super.appendHoverText(itemStack, level, list, tooltipFlag);
     }
 }
