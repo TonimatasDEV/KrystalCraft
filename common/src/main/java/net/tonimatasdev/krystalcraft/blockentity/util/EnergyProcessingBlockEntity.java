8732d1c677af3e09a8fd906fd3a4670959a405e1
TonimatasDEV <amatascubero@gmail.com>
2023-08-06 23:12:57 +0200

    API for better energy

---
+++ common/src/main/java/net/tonimatasdev/krystalcraft/blockentity/util/EnergyProcessingBlockEntity.java
@@ -0,0 +1,34 @@
+package net.tonimatasdev.krystalcraft.blockentity.util;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+public abstract class EnergyProcessingBlockEntity extends EnergyBlockEntity {
+    protected int progress;
+
+    public EnergyProcessingBlockEntity(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState) {
+        super(blockEntityType, blockPos, blockState);
+    }
+
+    @Override
+    public void load(CompoundTag compoundTag) {
+        super.load(compoundTag);
+        this.progress = compoundTag.getInt("Progress");
+    }
+
+    @Override
+    public void saveAdditional(CompoundTag compoundTag) {
+        super.saveAdditional(compoundTag);
+        compoundTag.putInt("Progress", this.progress);
+    }
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public int getMaxProgress() {
+        return 20;
+    }
+}
