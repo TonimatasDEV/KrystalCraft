242f62766bd977a4891cabc0ce545f2c6edd4237
TonimatasDEV <amatascubero@gmail.com>
2023-08-11 05:06:44 +0200

    Fix small things

--- common/src/main/java/net/tonimatasdev/krystalcraft/block/MachineBlock.java
+++ common/src/main/java/net/tonimatasdev/krystalcraft/block/MachineBlock.java
@@ -2,6 +2,7 @@ package net.tonimatasdev.krystalcraft.block;
 
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.tonimatasdev.krystalcraft.block.util.AbstractMachineBlock;
 
 public class MachineBlock extends AbstractMachineBlock {
     public MachineBlock() {
