0c9b95a6be186b32d94deca3b1f5940124dd70f7
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-09-17 12:46:01 +0200

    Jei compat

--- common/src/main/java/net/tonimatasdev/krystalcraft/compat/jei/CuttingStationRecipeCategory.java
+++ common/src/main/java/net/tonimatasdev/krystalcraft/compat/jei/CuttingStationRecipeCategory.java
@@ -1,6 +1,6 @@
 package net.tonimatasdev.krystalcraft.compat.jei;
 
-/*
+
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
@@ -12,30 +12,28 @@ import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.tonimatasdev.krystalcraft.KrystalCraft;
-import net.tonimatasdev.krystalcraft.recipe.CuttingStationRecipe;
+import net.tonimatasdev.krystalcraft.recipe.CuttingRecipe;
 import net.tonimatasdev.krystalcraft.registry.ModBlocks;
-import net.tonimatasdev.krystalcraft.registry.ModItems;
 import org.jetbrains.annotations.NotNull;
 
-public class CuttingStationRecipeCategory implements IRecipeCategory<CuttingStationRecipe> {
+public class CuttingStationRecipeCategory implements IRecipeCategory<CuttingRecipe> {
     private final IDrawable background;
     private final IDrawable icon;
 
     public CuttingStationRecipeCategory(IGuiHelper helper) {
-        this.background = helper.createDrawable(new ResourceLocation(KrystalCraft.MOD_ID, "textures/gui/gem_cutting_station_gui.png"), 0, 0, 176, 85);
+        this.background = helper.createDrawable(new ResourceLocation(KrystalCraft.MOD_ID, "textures/gui/cutting_station.png"), 0, 0, 176, 85);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.CUTTING_STATION.get()));
     }
 
     @Override
-    public @NotNull RecipeType<CuttingStationRecipe> getRecipeType() {
-        return JEIKrystalCraftModPlugin.CUTTING_STATION;
+    public @NotNull RecipeType<CuttingRecipe> getRecipeType() {
+        return JEIKrystalCraftModPlugin.CUTTING;
     }
 
     @Override
     public @NotNull Component getTitle() {
-        return Component.literal("Gem Cutting Station");
+        return Component.literal("Cutting");
     }
 
     @Override
@@ -49,11 +47,8 @@ public class CuttingStationRecipeCategory implements IRecipeCategory<CuttingStat
     }
 
     @Override
-    public void setRecipe(IRecipeLayoutBuilder builder, CuttingStationRecipe recipe, IFocusGroup focusGroup) {
-        builder.addSlot(RecipeIngredientRole.INPUT, 34, 40).addIngredients(Ingredient.of(ModItems.SET_WATER_BOTTLES.get()));
+    public void setRecipe(IRecipeLayoutBuilder builder, CuttingRecipe recipe, IFocusGroup focusGroup) {
         builder.addSlot(RecipeIngredientRole.INPUT, 57, 18).addIngredients(recipe.getIngredients().get(0));
-        builder.addSlot(RecipeIngredientRole.INPUT, 103, 18).addIngredients(Ingredient.of(ModItems.GEM_CUTTER_TOOL.get()));
         builder.addSlot(RecipeIngredientRole.OUTPUT, 80, 60).addItemStack(recipe.getOutput());
     }
 }
-*/
